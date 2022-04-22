35c0e3b1e638afc4f53fe558559f4fb2b9e4b8da
afifputra <afifputraharyono@gmail.com>
2022-04-22 15:41:50 +0700

    try use ref and portal

--- react-project-section-8/src/components/UI/ErrorModal.js
+++ react-project-section-8/src/components/UI/ErrorModal.js
@@ -1,23 +1,35 @@
+import ReactDOM from "react-dom";
+
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onCloseModal} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onCloseModal}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onCloseModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onCloseModal}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(<Backdrop onCloseModal={props.onCloseModal} />, document.getElementById("backdrop-root"))}
+      {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onCloseModal={props.onCloseModal} />, document.getElementById("overlay-root"))}
+    </>
   );
 };
 
--- react-project-section-8/src/components/Users/AddUser.js
+++ react-project-section-8/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -6,20 +6,25 @@ import classes from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
+  const enteredName = useRef("");
+  const enteredUserAge = useRef("");
+
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const name = enteredName.current.value;
+    const age = enteredUserAge.current.value;
+    if (name.trim().length === 0 || age.trim().length === 0) {
       setError({
         title: "Invalid input!",
         message: "Please enter a valid username and age!",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+age < 1) {
       setError({
         title: "Invalid age!",
         message: "Please enter a valid age! (1 or higher)",
@@ -28,39 +33,41 @@ const AddUser = (props) => {
     }
     const user = {
       id: Math.random().toString(),
-      name: enteredUsername,
-      age: enteredAge,
+      name: name,
+      age: age,
     };
     props.onAddUser(user);
-    setEnteredUsername("");
-    setEnteredAge("");
+    enteredName.current.value = "";
+    enteredUserAge.current.value = "";
+    // setEnteredUsername("");
+    // setEnteredAge("");
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const modalCloseHandler = () => {
     setError();
   };
 
   return (
-    <div>
+    <>
       {error && <ErrorModal title={error.title} message={error.message} onCloseModal={modalCloseHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" onChange={usernameChangeHandler} value={enteredUsername} />
+          <input type="text" name="username" id="username" ref={enteredName} />
           <label htmlFor="age">Age</label>
-          <input type="number" name="age" id="age" onChange={ageChangeHandler} value={enteredAge} />
+          <input type="number" name="age" id="age" ref={enteredUserAge} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </>
   );
 };
 

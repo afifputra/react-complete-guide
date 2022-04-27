b164507eb6afdd65094f05b486f31155da0f3616
afifputra <afifputraharyono@gmail.com>
2022-04-27 11:04:10 +0700

    make meals component

---
+++ react-food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvalaibleMeals from "./AvailableMeals";
+
+const Meals = (props) => {
+  return (
+    <>
+      <MealsSummary />
+      <AvalaibleMeals />
+    </>
+  );
+};
+
+export default Meals;

534f90e38ad057cf45ac3746c843e0573cf24296
afifputra <afifputraharyono@gmail.com>
2022-04-30 11:00:26 +0700

    add reducer to cart

--- react-food-order-app/src/context/CartProvider.js
+++ react-food-order-app/src/context/CartProvider.js
@@ -1,9 +1,46 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const initialState = {
+  item: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state,
+        item: [...state.item, action.item],
+        totalAmount: state.totalAmount + action.item.price * action.item.amount,
+      };
+    case "REMOVE_ITEM":
+      return {
+        ...state,
+        item: state.item.filter((item) => item.id !== action.id),
+        totalAmount: state.totalAmount - action.item.price * action.item.amount,
+      };
+    default:
+      return state;
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, initialState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCart({
+      type: "ADD_ITEM",
+      item: item,
+    });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCart({
+      type: "REMOVE_ITEM",
+      id: id,
+    });
+  };
 
   const cartContext = {
     item: [],

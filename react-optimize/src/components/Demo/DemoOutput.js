30072760ce05e538765a4e473f8a207c184bb010
afifputra <afifputraharyono@gmail.com>
2022-05-09 11:01:56 +0700

    using memo

---
+++ react-optimize/src/components/Demo/DemoOutput.js
@@ -0,0 +1,8 @@
+import { memo } from "react";
+
+const DemoOutput = (props) => {
+  console.log("DEMO OUTPUT RUNNING");
+  return <p>{props.show ? "This is new!" : ""}</p>;
+};
+
+export default memo(DemoOutput);

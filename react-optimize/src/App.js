f923eadd145dc66792a66dbe9b2f12622e933259
afifputra <afifputraharyono@gmail.com>
2022-05-09 11:58:19 +0700

    useCallback

--- react-optimize/src/App.js
+++ react-optimize/src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import Button from "./components/UI/Button/Button";
-import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+
   console.log("APP RUNNING");
-  const toggleParagraph = () => {
+
+  const toggleParagraph = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
